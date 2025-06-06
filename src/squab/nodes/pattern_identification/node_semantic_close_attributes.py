e15020b1b19da5788c2d89a227c6001632caf007
spapicchio <simopapi@hotmail.com>
2025-06-06 19:07:51 +0200

    fix: use litellm to get cost for encoder

--- src/squab/nodes/pattern_identification/node_semantic_close_attributes.py
+++ src/squab/nodes/pattern_identification/node_semantic_close_attributes.py
@@ -1,17 +1,12 @@
 import copy
-import os
 
-from dotenv import load_dotenv
-from langchain_community.callbacks import get_openai_callback
-from langchain_openai import OpenAIEmbeddings
+import litellm
 from langgraph.func import task
 
 from squab.graph_states import Line
 from squab.nodes.generation_steps import GenerationSteps
 from squab.nodes.pattern_identification.utils import utils_get_top_k_index_similar_matrix, utils_combine_clusters
 from squab.nodes.utils import utils_check_previous_step, utils_get_columns_no_pk_fk
-
-load_dotenv(override=True)
 
 
 @task
@@ -24,10 +19,6 @@ def node_semantic_close_attributes(
 ) -> list[Line]:
     # check if the previous step has been executed
     utils_check_previous_step(dataset, GenerationSteps.PI)
-    encoder = OpenAIEmbeddings(
-        model="text-embedding-3-large",
-        api_key=os.getenv('OPENAI_API_KEY')
-    )
     processed_dataset = []
     for line in dataset:
         lines = copy.deepcopy(line)
@@ -40,27 +31,26 @@ def node_semantic_close_attributes(
                                                     " cannot find a pattern."
                 }
             else:
-                lines = _process_line(lines, columns_no_pk_fk, encoder, threshold_similar_values)
+                lines = _process_line(lines, columns_no_pk_fk, encoder_name, threshold_similar_values)
 
         lines = lines if isinstance(lines, list) else [lines]
         processed_dataset.extend(lines)
     return processed_dataset
 
 
-def _process_line(line: Line, columns_no_pk_fk, encoder, threshold_similar_values) -> list[Line]:
-    with get_openai_callback() as cb:
-        similar_columns = _get_similar_values(
-            columns_no_pk_fk,
-            threshold_similar_values=threshold_similar_values,
-            encoder=encoder
-        )
+def _process_line(line: Line, columns_no_pk_fk, encoder_name, threshold_similar_values) -> list[Line]:
+    similar_columns, total_cost = _get_similar_values(
+        columns_no_pk_fk,
+        threshold_similar_values=threshold_similar_values,
+        encoder_name=encoder_name
+    )
     processed_lines = []
     for column_pairs in similar_columns:
         if len(column_pairs) > 1:
             processed_line = copy.deepcopy(line)
             # update the costs
-            processed_line['granular_costs']['pattern_identification'] = cb.total_cost
-            processed_line['total_cost'] += cb.total_cost
+            processed_line['granular_costs']['pattern_identification'] = total_cost
+            processed_line['total_cost'] += total_cost
             processed_line['pattern_identification'] = {"similar_columns": column_pairs}
             processed_lines.append(processed_line)
     return processed_lines
@@ -69,13 +59,15 @@ def _process_line(line: Line, columns_no_pk_fk, encoder, threshold_similar_value
 def _get_similar_values(
         values: list[str],
         threshold_similar_values,
-        encoder,
-) -> list[list[str]]:
+        encoder_name,
+) -> tuple[list[list[str]], float]:
     at_most_k = int(len(values) / 2)
     at_most_k = 2 if at_most_k < 2 else at_most_k
     parsed_columns = [f'{col}' for col in values]
 
-    vals_embeddings = encoder.embed_documents(parsed_columns)
+    vals_embeddings = litellm.embedding(model=encoder_name, input=parsed_columns)
+    total_cost = litellm.completion_cost(vals_embeddings)
+    vals_embeddings = [val['embedding'] for val in vals_embeddings.data]
 
     top_k_indexes = utils_get_top_k_index_similar_matrix(vals_embeddings,
                                                          at_most_k=at_most_k,
@@ -89,4 +81,4 @@ def _get_similar_values(
     # combine the values which are subsets of each other
     cluster_name2similar_cols = utils_combine_clusters(cluster_name2similar_cols)
 
-    return list(cluster_name2similar_cols.values())
+    return list(cluster_name2similar_cols.values()), total_cost
