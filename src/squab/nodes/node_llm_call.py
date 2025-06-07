c3d1e01e4f516fc8172bccb9a589d8f0cf70590f
spapicchio <simopapi@hotmail.com>
2025-06-07 12:54:27 +0200

    feat: add generic llm_node_update_line to avoid code duplication and update node_hypernym.py. Removed deprecated node_process_dataset.py

--- src/squab/nodes/node_llm_call.py
+++ src/squab/nodes/node_llm_call.py
@@ -1,12 +1,26 @@
 import litellm
+from jinja2 import Template
 from langgraph.func import task
 from litellm.types.utils import ModelResponse
 
-from squab.nodes.utils import is_openai_format
+from squab.graph_states import Line
+from squab.nodes.utils import is_openai_format, utils_get_last_json_from_text, GenerationSteps
+
+
+def _create_messages(system: str | None, user: str, few_shots: list[dict], **kwargs):
+    few_shots_messages = []
+    for shot in few_shots:
+        few_shots_messages.extend([
+            {"role": "user", "content": Template(user).render(**shot)},
+            {"role": "assistant", "content": shot['assistant_answer']}
+        ])
+    system_message = [{"role": "system", "content": system}] if system is not None else []
+    user_message = [{"role": "user", "content": Template(user).render(**kwargs)}]
+    return system_message + few_shots_messages + user_message
 
 
 @task
-def llm_call(messages, litellm_params) -> tuple[ModelResponse, float]:
+def llm_call(messages: list, litellm_params: dict) -> tuple[ModelResponse, float]:
     if not is_openai_format:
         raise ValueError(f"Only OpenAI format is supported. Received messages: {messages}")
     response = litellm.completion(
@@ -15,3 +29,28 @@ def llm_call(messages, litellm_params) -> tuple[ModelResponse, float]:
     )
     cost = litellm.completion_cost(response)
     return response, cost
+
+
+@task
+def llm_node_update_line(line: Line,
+                         system: str | None,
+                         user: str,
+                         few_shots: list[dict],
+                         col_to_update: str,
+                         litellm_params: dict,
+                         template_params: dict,
+                         step: GenerationSteps) -> Line:
+    messages = _create_messages(system, user, few_shots, **template_params)
+    response, total_cost = llm_call(messages, litellm_params).result()
+    model_response = response["choices"][0]["message"]["content"]
+    generated_response = utils_get_last_json_from_text(model_response)
+    if not generated_response:
+        line['has_failed'] = {
+            step.value: f"Model Response: {model_response}"
+        }
+    else:
+        line[col_to_update] = generated_response
+
+    line['total_cost'] += total_cost
+    line['granular_costs'][step.value] = total_cost
+    return line
--- src/squab/nodes/relational_metadata/node_hypernym.py
+++ src/squab/nodes/relational_metadata/node_hypernym.py
@@ -1,54 +1,10 @@
-from jinja2 import Template
-from langgraph.func import task
-
 from squab.graph_states import Line
-from squab.nodes.node_llm_call import llm_call
-from squab.nodes.node_process_dataset import node_process_dataset
-from squab.nodes.utils import utils_get_last_json_from_text, GenerationSteps
+from squab.nodes.decorator_process_dataset import dataset_processor
+from squab.nodes.node_llm_call import llm_node_update_line
+from squab.nodes.utils import GenerationSteps
 
 
-@task
-def node_get_hypernym(
-        dataset: list[Line],
-        litellm_params_hypernym: dict,
-        hypernym_user_template: str | None = None,
-        hypernym_system_template: str | None = None,
-        *args, **kwargs
-) -> list[Line]:
-    """Generate hypernyms for the dataset using the provided templates and LLM parameters."""
-    return node_process_dataset(
-        dataset=dataset,
-        step=GenerationSteps.RM,
-        process_line_fn=process_hypernym_line,
-        litellm_params_hypernym=litellm_params_hypernym,
-        hypernym_user_template=hypernym_user_template,
-        hypernym_system_template=hypernym_system_template,
-        *args, **kwargs
-    ).result()
-
-
-HYPERNYM_SYSTEM_PROMPT = (
-    "Given a specific table schema and a designated set of columns, your task is to generate a suitable and appropriate label for that particular set. "
-    'A set is defined as a collection of column names that share a semantic relationship, which may include examples such as "First Name" and '
-    '"Last Name." The label you create should be a single term that effectively encompasses all the columns in that set, such as "Personal Information" or "Name." '
-    "It is crucial that the label must be unique within its set and the table schema and also relevant to the overall table schema in "
-    "question. As output, return a JSON enclosed in ```json ```. Instead, if there is no possible labeling solution, return an empty dictionary ``json {}```. "
-    "\n## Output\n\n"
-    "```json\n"
-    "{\n"
-    '  "label": "the label that represents the set of columns"\n'
-    "}\n"
-    "```\n"
-)
-
-HYPERNYM_TEMPLATE = """
-## Table Schema
-{{ tbl_schema }}
-## Semantic related columns
-{{ cols }}
-""".rstrip()
-
-
+@dataset_processor(GenerationSteps.RM)
 def process_hypernym_line(
         line: Line,
         litellm_params_hypernym: dict,
@@ -57,28 +13,14 @@ def process_hypernym_line(
         *args, **kwargs
 ) -> Line:
     """Process a single line for hypernym generation."""
-    system_prompt = hypernym_system_template or HYPERNYM_SYSTEM_PROMPT
-    user_template = hypernym_user_template or HYPERNYM_TEMPLATE
-
-    messages = [
-        {"role": "system", "content": system_prompt},
-        {"role": "user",
-         "content": Template(user_template).render(tbl_schema=line['db_schema_table_examples'],
-                                                   cols=line['pattern_identification'])}
-    ]
-
-    response, total_cost = llm_call(messages, litellm_params_hypernym).result()
-    model_response = response["choices"][0]["message"]["content"]
-    hypernym = utils_get_last_json_from_text(model_response)
-
-    if not hypernym:
-        line['has_failed'] = {
-            'rm_hypernym': f"The table has no hypernym, cannot find a pattern. Model Response: {model_response}"
-        }
-    else:
-        line['relational_metadata'] = hypernym
-
-    line['granular_costs']['relational_metadata'] = total_cost
-    line['total_cost'] += total_cost
-
+    line = llm_node_update_line(
+        line,
+        system=hypernym_system_template,
+        user=hypernym_user_template,
+        few_shots=[],
+        col_to_update=GenerationSteps.RM.value,
+        litellm_params=litellm_params_hypernym,
+        template_params={'tbl_schema': line['db_schema_table_examples'], 'cols': line[GenerationSteps.PI.value]},
+        step=GenerationSteps.RM,
+    ).result()
     return line
